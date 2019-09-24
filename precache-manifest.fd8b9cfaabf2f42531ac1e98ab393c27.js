e1579e82776b21c2e5443d853c73221aee04bc41
danish-ahmed <qureshidanish78@gmail.com>
2019-09-24 16:28:12 +0500

    Updates

---
+++ precache-manifest.fd8b9cfaabf2f42531ac1e98ab393c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "f19b40d7deccd0c93b93",
+    "url": "/react-gh-pages/static/js/main.e692c3ef.chunk.js"
+  },
+  {
+    "revision": "acc809110974037e04f4",
+    "url": "/react-gh-pages/static/js/2.5a561164.chunk.js"
+  },
+  {
+    "revision": "f19b40d7deccd0c93b93",
+    "url": "/react-gh-pages/static/css/main.d2bfa548.chunk.css"
+  },
+  {
+    "revision": "acc809110974037e04f4",
+    "url": "/react-gh-pages/static/css/2.ae9aa405.chunk.css"
+  },
+  {
+    "revision": "7efbf56caac852dfffe9901957af7320",
+    "url": "/react-gh-pages/index.html"
+  }
+];
